944a6eb0267f38b9d10557233a28acf63feb5728
Thomas Balthazar <thomas@balthazar.info>
2013-09-23 15:41:30 +0200

    basic xmpp connection + presence

---
+++ Cryptocat/TBXMPPManager.m
@@ -0,0 +1,255 @@
+//
+//  TBXMPPManager.m
+//  Cryptocat
+//
+//  Created by Thomas Balthazar on 23/09/13.
+//  Copyright (c) 2013 Thomas Balthazar. All rights reserved.
+//
+
+#import "TBXMPPManager.h"
+
+#import "XMPP.h"
+#import "XMPPReconnect.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+@interface TBXMPPManager () <XMPPStreamDelegate>
+
+@property (nonatomic, strong, readonly) XMPPStream *xmppStream;
+@property (nonatomic, strong, readonly) XMPPReconnect *xmppReconnect;
+
+- (void)goOnline;
+- (void)goOffline;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+@implementation TBXMPPManager
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Initializer
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (id)init {
+  if (self=[super init]) {
+    _xmppStream = [[XMPPStream alloc] init];
+    _xmppReconnect = [[XMPPReconnect alloc] init];
+
+    [_xmppReconnect activate:_xmppStream];
+    
+    [_xmppStream addDelegate:self delegateQueue:dispatch_get_main_queue()];
+  }
+  
+  return self;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)dealloc {
+  [_xmppStream removeDelegate:self];
+  [_xmppReconnect deactivate];
+  [_xmppStream disconnect];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Public Methods
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)connect {
+  TBLOGMARK;
+  if (!self.xmppStream.isDisconnected) return YES;
+  
+  NSString *myJID = @"tbalthazar@gmail.com";
+	[self.xmppStream setMyJID:[XMPPJID jidWithString:myJID]];
+  
+	NSError *error = nil;
+	if (![self.xmppStream connectWithTimeout:XMPPStreamTimeoutNone error:&error]) {
+		UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error connecting"
+		                                                    message:@"See console for error details."
+		                                                   delegate:nil
+		                                          cancelButtonTitle:@"Ok"
+		                                          otherButtonTitles:nil];
+		[alertView show];
+		TBLOG(@"Error connecting: %@", error);
+		return NO;
+	}
+  
+	return YES;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)disconnect {
+	[self goOffline];
+	[self.xmppStream disconnect];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Private Methods
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)goOnline {
+	XMPPPresence *presence = [XMPPPresence presence]; // type="available" is implicit
+	[self.xmppStream sendElement:presence];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)goOffline {
+	XMPPPresence *presence = [XMPPPresence presenceWithType:@"unavailable"];
+	[self.xmppStream sendElement:presence];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark XMPPStreamDelegate
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStream:(XMPPStream *)sender socketDidConnect:(GCDAsyncSocket *)socket {
+	TBLOGMARK;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStream:(XMPPStream *)sender willSecureWithSettings:(NSMutableDictionary *)settings {
+	TBLOGMARK;
+	
+//	if (allowSelfSignedCertificates) {
+//		[settings setObject:[NSNumber numberWithBool:YES]
+//                 forKey:(NSString *)kCFStreamSSLAllowsAnyRoot];
+//	}
+	
+	//if (allowSSLHostNameMismatch) {
+  if (NO) {
+		[settings setObject:[NSNull null] forKey:(NSString *)kCFStreamSSLPeerName];
+	}
+	else {
+		// Google does things incorrectly (does not conform to RFC).
+		// Because so many people ask questions about this (assume xmpp framework is broken),
+		// I've explicitly added code that shows how other xmpp clients "do the right thing"
+		// when connecting to a google server (gmail, or google apps for domains).
+		
+		NSString *expectedCertName = nil;
+		
+		NSString *serverDomain = self.xmppStream.hostName;
+		NSString *virtualDomain = [self.xmppStream.myJID domain];
+		
+		if ([serverDomain isEqualToString:@"talk.google.com"]) {
+			if ([virtualDomain isEqualToString:@"gmail.com"]) {
+				expectedCertName = virtualDomain;
+			}
+			else {
+				expectedCertName = serverDomain;
+			}
+		}
+		else if (serverDomain == nil) {
+			expectedCertName = virtualDomain;
+		}
+		else {
+			expectedCertName = serverDomain;
+		}
+		
+		if (expectedCertName) {
+			[settings setObject:expectedCertName forKey:(NSString *)kCFStreamSSLPeerName];
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStreamDidSecure:(XMPPStream *)sender {
+	TBLOGMARK;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStreamDidConnect:(XMPPStream *)sender {
+	TBLOGMARK;
+		
+	NSError *error = nil;
+  NSString *password = @"";
+	if (![self.xmppStream authenticateWithPassword:password error:&error]) {
+    TBLOG(@"Error authenticating : %@", error);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStreamDidAuthenticate:(XMPPStream *)sender {
+	TBLOGMARK;
+
+	[self goOnline];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(NSXMLElement *)error {
+	TBLOG(@"%@", error);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)xmppStream:(XMPPStream *)sender didReceiveIQ:(XMPPIQ *)iq {
+	TBLOG(@"didReceiveIQ : %@", iq);
+	
+	return NO;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message {
+	TBLOG(@"-- stream : %@ | message : %@", sender, message);
+  
+	// A simple example of inbound message handling.
+  /*
+	if ([message isChatMessageWithBody])
+	{
+		XMPPUserCoreDataStorageObject *user = [xmppRosterStorage userForJID:[message from]
+		                                                         xmppStream:xmppStream
+		                                               managedObjectContext:[self managedObjectContext_roster]];
+		
+		NSString *body = [[message elementForName:@"body"] stringValue];
+		NSString *displayName = [user displayName];
+    
+		if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateActive)
+		{
+			UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:displayName
+                                                          message:body
+                                                         delegate:nil
+                                                cancelButtonTitle:@"Ok"
+                                                otherButtonTitles:nil];
+			[alertView show];
+		}
+		else
+		{
+			// We are not active, so use a local notification instead
+			UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+			localNotification.alertAction = @"Ok";
+			localNotification.alertBody = [NSString stringWithFormat:@"From: %@\n\n%@",displayName,body];
+      
+			[[UIApplication sharedApplication] presentLocalNotificationNow:localNotification];
+		}
+	}
+  */
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStream:(XMPPStream *)sender didReceivePresence:(XMPPPresence *)presence {
+	TBLOG(@"Presence : %@", presence);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStream:(XMPPStream *)sender didReceiveError:(id)error {
+	TBLOG(@"-- XMPPStream error : %@", error);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)xmppStreamDidDisconnect:(XMPPStream *)sender withError:(NSError *)error {
+	TBLOG(@"-- stream did disconnect with error : %@", error);
+	
+//	if (!isXmppConnected) {
+//		DDLogError(@"Unable to connect to server. Check xmppStream.hostName");
+//	}
+}
+
+@end
