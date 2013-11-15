929febbc5b60f62e7c14779b948939b74fc3d9b8
Thomas Balthazar <thomas@balthazar.info>
2013-11-15 13:48:24 +0100

    Buddies List : don’t display the disclosure indicator in front of the Conversation cell

--- Cryptocat/TBBuddiesViewController.m
+++ Cryptocat/TBBuddiesViewController.m
@@ -103,6 +103,7 @@
                                                           forIndexPath:indexPath];
   
   if (indexPath.row==0) {
+    cell.accessoryType = UITableViewCellAccessoryNone;
     NSString *conversationRoomTitle = NSLocalizedString(@"Conversation",
                                                         @"Conversation Room Label");
     if (self.nbUnreadMessagesInRoom == 0) {
